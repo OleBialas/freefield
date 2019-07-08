045ffe0924f03880afff58b9106a169db83c56eb
Ole <bialas@cbs.mpg.de>
2019-07-08 13:11:09 +0200

    a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='FreefieldToolbox',
     version='0.1dev',
-    packages=['scripts',],
+    packages=['freefield',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )

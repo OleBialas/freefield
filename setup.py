c3459be39d3e846f0572cf73a9b85192b6d42917
Ole <bialas@cbs.mpg.de>
2019-07-10 16:46:57 +0200

    a

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='FreefieldToolbox',
+    name='freefield_toolbox',
     version='0.1dev',
-    packages=['FreefieldToolbox'],
+    packages=['freefield_toolbox'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )

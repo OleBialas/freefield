c274c34bf2fab6e4a50f8c963c29539488d7feb0
Ole Bialas <bialas.ole@gmx.de>
2020-10-21 08:58:45 +0200

    basic testroutine for mock devices

--- tests/test_device_io.py
+++ tests/test_device_io.py
@@ -7,3 +7,9 @@ def test_devices():
     for mode in ["play_rec", "play_birec", "loctest_freefield",
                  "loctest_headphones", "cam_calibration"]:
         _devices.initialize_default(mode)
+
+    for n_samples in [1, 1000]:
+        data = _devices.read("tag", n_samples, proc='RX8')
+        assert len(data) == n_samples
+
+    assert _devices.write("tag", 1, procs=['RX81', 'RX82']) == 1

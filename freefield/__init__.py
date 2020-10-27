6f3e267975fd3da29b19fda3b4494a2c2a0ee557
Ole Bialas <bialas.ole@gmx.de>
2020-10-27 13:52:11 +0100

    corrrect testdir

--- freefield/__init__.py
+++ freefield/__init__.py
@@ -3,7 +3,9 @@ import sys
 __version__ = '0.1'
 
 sys.path.append('..\\')
-DATADIR = pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')
-TESTDIR = pathlib.Path(__file__).parent.resolve() / pathlib.Path('tests')
+DATADIR = \
+    pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')
+TESTDIR = \
+    pathlib.Path(__file__).parent.parent.resolve() / pathlib.Path('tests')
 
 from freefield.devices import Devices

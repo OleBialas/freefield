225e5d987c8bc3ec72a1006cb3871076e6a40aef
Ole Bialas <bialas.ole@gmx.de>
2020-10-27 13:34:46 +0100

    add testdir

--- freefield/__init__.py
+++ freefield/__init__.py
@@ -4,5 +4,6 @@ __version__ = '0.1'
 
 sys.path.append('..\\')
 DATADIR = pathlib.Path(__file__).parent.resolve() / pathlib.Path('data')
+TESTDIR = pathlib.Path(__file__).parent.resolve() / pathlib.Path('tests')
 
 from freefield.devices import Devices

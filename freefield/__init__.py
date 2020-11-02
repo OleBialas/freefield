da0ebf6e640bcf965b3460ed7dca3995b56d2926
Ole Bialas <bialas.ole@gmx.de>
2020-11-02 15:50:32 +0100

    fix import order

--- freefield/__init__.py
+++ freefield/__init__.py
@@ -6,5 +6,5 @@ sys.path.append('..\\')
 DIR = pathlib.Path(__file__).parent.resolve()
 
 from freefield.devices import Devices
-from freefield.camera import Cameras
 from freefield.headpose import PoseEstimator
+from freefield.camera import Cameras

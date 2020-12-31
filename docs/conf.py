91838423f1f09ab63adee6ed4fe9e030030f4a29
Ole Bialas <bialas.ole@gmx.de>
2020-12-31 15:44:52 +0100

    add ipython directive

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,9 @@ needs_sphinx = '1.8'
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
-    'matplotlib.sphinxext.plot_directive']
+    'matplotlib.sphinxext.plot_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

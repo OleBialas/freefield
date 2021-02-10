48153543a79506d970b3b3e11f2899a389b27ffb
OleBialas <bialas.ole@gmx.de>
2021-02-10 11:18:53 +0100

    add routine to plot speaker positions

--- freefield/visualizations.py
+++ freefield/visualizations.py
@@ -1,4 +1,4 @@
-# from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from math import *
@@ -25,119 +25,29 @@ def plot_face_detection_marks(image):
     plt.scatter(marks[:, 0], marks[:, 1], color="red", marker=".")
 
 
-def plot_sources():
+def plot_sources(azimuth, elevation, distance=1.6):
+    """Display sources in a 3D plot
+    Arguments:
+        azimuth (np.ndarray): azimuth of the sources in degree. Must be same length as elevation
+        elevation (np.ndarray): elevation of the sources in degree. Must be same length as azimuth
+        distance (float | np.ndarray): distance of the sources to the listener. Can be either an array
+            with the same length as azimuth and elevation or a single float if all sources have the same
+            distance - meaning they are arranged in a sphere or circle. """
+    ax = Axes3D(plt.figure())
+    azimuth = np.deg2rad(azimuth)
+    elevation = np.deg2rad(elevation-90)
+    x = distance * np.sin(elevation) * np.cos(azimuth)
+    y = distance * np.sin(elevation) * np.sin(azimuth)
+    z = distance * np.cos(elevation)
+    ax.scatter(x, y, z, c='b', marker='.')
+    ax.scatter(0, 0, 0, c='r', marker='o')
 
-    pass
-
-"""
-def plot_dome(speakers="all"):
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_aspect("auto")
-    # draw sphere
-    u, v = np.mgrid[0:2 * np.pi:20j, 0:np.pi:10j]
-    x = np.cos(u) * np.sin(v)
-    y = np.sin(u) * np.sin(v)
-    z = np.cos(v)
-    ax.plot_wireframe(x, y, z, color="r")
-
-    # plot point representing the head of the listener, at the origin of the dome
-    ax.scatter([0], [0], [0], color="black", s=100)
-    # plot arrow representing gaze direction:
-    a = Arrow3D([0, 1], [0, 0], [0, 0], mutation_scale=20,
-                lw=1, arrowstyle="-|>", color="k")
-    ax.add_artist(a)
-
-    # draw a point for each speaker:
-    speakers = main.read_table("dome")
-    speakers = speakers[["azi", "ele"]]
-    speakers = speakers.where(speakers["azi"] == 0.0).dropna()
-    speakers = speakers.append(pd.DataFrame([[0.0, 55.5], [0.0, -55.5]], columns=["azi", "ele"]))
-    for index, row in speakers.iterrows():
-        spherical_point = SphericalPoint(r=1, theta=row.ele, phi=row.azi)
-        print(spherical_point)
-        print(spherical_point.degrees())
-
-        point = spherical_point.to_cartesian()
-        print(point)
-        ax.scatter([point.x], [point.y], [point.z], color="green", s=100)
-
-    plt.show()
-
-
-class Arrow3D(FancyArrowPatch):
-
-    def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
-        self._verts3d = xs, ys, zs
-
-    def draw(self, renderer):
-        xs3d, ys3d, zs3d = self._verts3d
-        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
-        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
-        FancyArrowPatch.draw(self, renderer)
-
-
-class Point(object):
-    def __init__(self, x=0.0, y=0.0, z=0.0):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __str__(self):
-        return '(%0.4f, %0.4f, %0.4f)' % (self.x, self.y, self.z)
-
-    def __repr__(self):
-        return 'Point(%f, %f, %f)' % (self.x, self.y, self.z)
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
-
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
-
-    def __mul__(self, f):
-        return Point(self.x * f, self.y * f, self.z * f)
-
-    def dist(self, other):
-        p = self - other
-        return (p.x ** 2 + p.y ** 2 + p.z ** 2) ** 0.5
-
-    def to_spherical(self):
-        r = self.dist(Point(0, 0, 0))
-        theta = atan2(hypot(self.x, self.y), self.z)
-        phi = atan2(self.y, self.x)
-        return SphericalPoint(r, theta, phi)
-
-
-class SphericalPoint(object):
-    def __init__(self, r, theta, phi):
-        # radial coordinate, zenith angle, azimuth angle
-        self.r = r
-        self.theta = theta
-        self.phi = phi
-
-    def degrees(self):
-        return 'SphericalPoint(%.4f, %.4f deg, %.4f deg)' % (self.r, degrees(self.theta) % 360, degrees(self.phi) % 360)
-
-    def __str__(self):
-        return '(%0.4f, %0.4f, %0.4f)' % (self.r, self.theta, self.phi)
-
-    def __repr__(self):
-        return 'SphericalPoint(%f, %f, %f)' % (self.r, self.theta, self.phi)
-
-    def to_cartesian(self):
-        x = self.r * cos(self.phi) * sin(self.theta)
-        y = self.r * sin(self.phi) * sin(self.theta)
-        z = self.r * cos(self.theta)
-        return Point(x, y, z)
-"""
 
 if __name__ == '__main__':
     speakers = main.read_table("dome")
-    speakers = speakers[["azi", "ele"]]
-
+    azi = np.array(speakers["azi"].to_list())
+    ele = np.array(speakers["ele"].to_list())
+    plot_sources(azi, ele)
 
 # def _plot_equalization(target, signal, filt, speaker_nr, low_cutoff=50,
 #                        high_cutoff=20000, bandwidth=1/8):
